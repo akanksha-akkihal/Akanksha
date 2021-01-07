6ae1f4326e5db089ab4b13956a3d43967d08c817
Akanksha27 <akankshaakkihal27@gmail.com>
2021-01-07 20:15:10 +0530

    final changes

--- script.js
+++ script.js
@@ -5,6 +5,28 @@ const nav=document.querySelector(".nav"),
 for(let i=0; i<totalNavList; i++){
   const a=navList[i].querySelector("a");
   a.addEventListener("click",function(){
+    for(let j=0; j<totalNavList;j++){
+      navList[j].querySelector("a").classList.remove('active');
+    }
     this.classList.add('active');
+      })
+}
+
+
+const navTogglerBtn=document.querySelector(".nav-toggler"),
+      aside=document.querySelector(".aside");
+navTogglerBtn.addEventListener("click",()=>{
+  asideSectionTogglerBtn();
+})
+
+function asideSectionTogglerBtn(){
+  aside.classList.toggle("open");
+  navTogglerBtn.classList.toggle("open");
+}
+
+for(let i=0; i<totalNavList; i++){
+  const a=navList[i].querySelector("a");
+  a.addEventListener("click",function(){
+    asideSectionTogglerBtn();
       })
 }
